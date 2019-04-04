431de8458b42ef92bd8c10a68e45a1b8f686a170
Nick Khaetsky <rzzsapb@gmail.com>
2019-04-04 12:55:37 +0300

    Update Feedback.php

--- components/Feedback.php
+++ components/Feedback.php
@@ -30,36 +30,36 @@ class Feedback extends ComponentBase
     public function onSend()
     {
         // Проверка
-        if(!Input::has('name')){
-            throw new AjaxException([ 'X_OCTOBER_ERROR_MESSAGE' => 'Вы должны указать свое имя' ]);
+        if(!Input::has('name') || empty(Input::get('name'))){
+            throw new \AjaxException([ 'X_OCTOBER_ERROR_MESSAGE' => 'Вы должны указать свое имя' ]);
         }
-
-        if(!Input::has('email')){
-            throw new AjaxException([ 'X_OCTOBER_ERROR_MESSAGE' => 'Вы должны указать email для связи' ]);
+    
+        if(!Input::has('email') || empty(Input::get('email'))){
+            throw new \AjaxException([ 'X_OCTOBER_ERROR_MESSAGE' => 'Вы должны указать email для связи' ]);
         }
-
+    
         // Составление массива
         $data = [
             'name'       => e(Input::get('name')),
             'email'      => e(Input::get('email')),
             'item_name'  => e(Input::get('item_name')),
         ];
-
+    
         if(Input::has('phone')){
             $data['phone'] = e(Input::get('phone'));
         }
-
+    
         // Отправка уведомления
         $email = $this->property('email');
         Mail::send('octoclub.tutorial::mail.feedback', $data, function($message) use ($email) {
             $message->to($email, 'Admin Person');
         });
-        
+    
         // Проверка успешно ли ушло письмо
         if (count(Mail::failures()) == 0){
             Flash::success( 'Форма успешно отправлена!' );
         } else {
-            throw new AjaxException([ 'X_OCTOBER_ERROR_MESSAGE' => 'Произошла ошибка, попробуйте позже' ]);
+            throw new \AjaxException([ 'X_OCTOBER_ERROR_MESSAGE' => 'Произошла ошибка, попробуйте позже' ]);
         }
     }
 }
